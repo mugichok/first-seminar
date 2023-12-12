95f9af90cf099d6f247cf8c08a11cfcea7e95d45
gendfrodit <farengate20017@gmail.com>
2023-12-12 22:02:57 +0300

    homework complete

---
+++ homework/Program.cs
@@ -0,0 +1,54 @@
+﻿// первая задача
+
+// Console.Clear();
+// Console.Write("Введите первое число ");
+// int x = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число ");
+// int y = int.Parse(Console.ReadLine()!);
+// if(x > y)
+//     Console.WriteLine($"{x} большее");
+
+// else if (y>x)
+//     Console.WriteLine($"{y} большее");
+
+// else
+//     Console.WriteLine("Числа равны");
+
+// вторая задача
+// Console.Clear();
+// Console.Write("Введите первое число ");
+// int x = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число ");
+// int y = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите третье число ");
+// int z = int.Parse(Console.ReadLine()!);
+// int max = x;
+// if(y > x && y > z)
+//     max = y;
+
+// else if (z > x)
+//     max = z;
+
+// Console.Write(max);
+
+//третья задача
+// Console.Clear();
+// Console.Write("Введите число ");
+// int x = int.Parse(Console.ReadLine()!);
+// if(x%2==0)
+//     Console.Write("Число чётное");
+// else
+//     Console.Write("Число нечётное");
+
+//четвёртая задача
+// Console.Clear();
+// Console.Write("Введите число ");
+// int x = int.Parse(Console.ReadLine()!);
+// for(int i = x*(-1); i <= x; i++)
+// {
+//     if(i%2==0)
+//     {
+//     Console.Write(i);
+//     }
+// }
+
